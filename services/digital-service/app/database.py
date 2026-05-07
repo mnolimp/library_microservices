03e12c764971bd3341b706570dc48f48d1e5ee11
malvia <mnolimp@mail.ru>
2026-05-07 23:26:11 +0600

    feat: dabase creation and connection

---
+++ services/digital-service/app/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SCHEMA = "digital" 
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set!")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+async_session_maker = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with async_session_maker() as session:
+        await session.execute(f"SET search_path TO {SCHEMA}")
+        yield session
