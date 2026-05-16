d4dfaf6a5a431014dbb69f4f1224f2326fc3107d
malvia <37767648+mnolimp@users.noreply.github.com>
2026-05-16 12:02:53 +0600

    fix: new db conn method for grpc

--- services/catalog-service/app/database.py
+++ services/catalog-service/app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import text
+from contextlib import asynccontextmanager
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -27,4 +28,9 @@ async def get_db():
     async with async_session_maker() as session:
         # Устанавливаем схему для сессии
         await session.execute(text(f"SET search_path TO {SCHEMA}"))
+        yield session
+
+@asynccontextmanager
+async def get_db_context():
+    async with async_session_maker() as session:
         yield session
